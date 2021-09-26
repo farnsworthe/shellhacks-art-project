672e035e4ab2fc300ec09cc7d453cb94520b4af8
farnsworthe <farnsworthe@knights.ucf.edu>
2021-09-25 20:20:06 -0400

    upload folder

---
+++ ml.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+
+from kaggle_datasets import KaggleDatasets
+import matplotlib.pyplot as plt
+import numpy as np
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+    
+print(tf.__version__)
+
